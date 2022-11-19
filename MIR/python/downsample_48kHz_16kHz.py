0083d9217960e47e9bcedfaa92b6d57be58c85f3
Donghyun Kim <82129086+utahboy3502@users.noreply.github.com>
2022-11-19 12:38:34 +0900

    Delete MIR directory

--- MIR/python/downsample_48kHz_16kHz.py
+++
@@ -1,9 +0,0 @@
-from pydub import AudioSegment as am
-import os
-
-input_filepath = ''
-output_filepath = ''
-
-sound = am.from_file(input_filepath, format='wav', frame_rate=48000)
-sound = sound.set_frame_rate(16000)
-sound.export(output_filepath, format='wav')
